29da15643405b3495119fdb56ec5fc87604e5780
Joel Fischer <jfjoelfischer@gmail.com>
2020-10-15 20:25:00 +0200

    Add automation for moving the due date into the future

---
+++ DueSelectedTasks.omnifocusjs/Resources/moveDueDay.js
@@ -0,0 +1,51 @@
+(() => Object.assign( 
+    new PlugIn.Action(selection => {
+
+        // main :: IO ()
+        const main = () => {
+            // addDays :: Int -> Date -> Date
+            const addDays = n => date => {
+                return (
+                    date.setDate(n + date.getDate()),
+                    date
+                );
+            };
+
+            // addWeeks :: Int -> Date -> Date
+            const addWeeks = n => addDays(7 * n);
+            
+            // addMonths :: Int -> Date -> Date
+            const addMonths = n => date => {
+                return (
+                    date.setMonth(n + d.getMonth()),
+                    date
+                )
+            };
+
+            const 
+                ts = selection
+                    .tasks
+                    .filter(task => null !== task.dueDate);
+
+            return ts.map(task => {
+                    const
+                        taskDate = task.dueDate;
+                    return (
+                        task.dueDate = addDays(1)(taskDate),
+                        task
+                    )
+                })
+        };
+
+        // MAIN -----------------------------------------
+        return main()
+        
+        }), {
+            validate: selection => 
+                selection
+                .tasks
+                .filter(task => null !== task.dueDate)
+                .length > 0
+        }
+    )   
+)();
